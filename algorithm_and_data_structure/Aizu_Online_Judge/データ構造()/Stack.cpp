1555e5586eb9de780388c399c8c24901dc386a0e
kaiTanobe <stomp0630@icloud.com>
2021-08-23 21:18:13 +0900

    Stackのコード書き換え一応終了。しかし stringstream ss{str};   と getline(); を使用して全コード書き換えるのもあり。

--- algorithm_and_data_structure/Aizu_Online_Judge/データ構造()/Stack.cpp
+++ algorithm_and_data_structure/Aizu_Online_Judge/データ構造()/Stack.cpp
@@ -7,102 +7,101 @@
 
 #include<iostream>
 #include<string>
+#include <sstream>
 #include<cstdlib>
 using namespace std;
-#define rep(i,n) for(int i=0;i<n;i++
+//#define rep(i,n) for(int i=0;i<n;i++
 
 
 class Stack{
 private:
-  int inf[1000];
-  int tp;
-
+    int inf[1000];
+    int tp;
+    
 public:
-
-  Stack(){
-    tp=0;
-  }
-
-
-  void push(int x){
-    tp++;
-    inf[tp] = x;
-  }
-
-  void pop(){
-    tp--;
-  }
-
-  int top(){
-    int x = inf[tp];
-    return x;
-  }
-
-  bool isempty(){
-    if(tp == 0)return true;
-    return false;
-  }
-
-  int size(){
-    return tp;
-  }
-
+    
+    Stack(){
+        tp = 0;
+    }
+    
+    
+    void push(int x){
+        tp ++;
+        inf[tp] = x;
+    }
+    
+    void pop(){
+        tp --;
+    }
+    
+    int top(){
+        int x = inf[tp];
+        return x;
+    }
+    
+    bool isempty(){
+        if (tp == 0) return true;
+        return false;
+    }
+    
+    int size(){
+        return tp;
+    }
+    
 };
 
- 
+
 
 
 void Stack() {
     class Stack stack;
     string str,s[10000];
-    int ind=0,th=0;
+    int ind = 0,th = 0;
     
-    getline(cin,str);
-    for(int i=0;i<str.length();i++){
-      
-      if(str[i] == ' ')
-        {
-      th=0;
-      ind++;
-        }    else
-        {
-      
-      s[ind].insert(th,str.substr(i,1));
-      th++;
+    stringstream ss{str};
+    getline(cin, str);
+    for(int i = 0; i < str.length() ;i++){
+        
+        if(str[i] == ' ') {
+            th=0;
+            ind++;
+        } else {
+            s[ind].insert(th,str.substr(i,1));
+            th++;
         }
     }
     ind++;
-
-    for(int i=0;i<ind;i++){
-         
-      if(s[i] == "+" || s[i] == "*" || s[i] == "-")
+    
+    for(int i = 0  ;i<ind;i++){
+        
+        if(s[i] == "+" || s[i] == "*" || s[i] == "-")
         {
-      
-      int x2 = stack.top();
-      stack.pop();
-      int x1 = stack.top();
-      stack.pop();
-      if(s[i] == "+")
-        {
-          stack.push(x1+x2);
+            
+            int x2 = stack.top();
+            stack.pop();
+            int x1 = stack.top();
+            stack.pop();
+            if(s[i] == "+")
+            {
+                stack.push(x1+x2);
+            }
+            else if(s[i] == "-")
+            {
+                stack.push(x1-x2);
+            }
+            else if(s[i] == "*")
+            {
+                stack.push(x1*x2);
+            }
         }
-      else if(s[i] == "-")
+        else
         {
-          stack.push(x1-x2);
-        }
-      else if(s[i] == "*")
-        {
-          stack.push(x1*x2);
-        }
-        }
-      else
-        {
-      if(s[i] == " ")continue;
-      int n = atoi(s[i].c_str());
-      stack.push(n);
+            if(s[i] == " ")continue;
+            int n = atoi(s[i].c_str());
+            stack.push(n);
         }
     }
     cout << stack.top() << endl;
-
-
+    
+    
 }
