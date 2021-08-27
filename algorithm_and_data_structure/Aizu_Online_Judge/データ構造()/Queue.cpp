a3524438122298c22a5003d8f4c61b9622d038a3
kaiTanobe <stomp0630@icloud.com>
2021-08-27 21:20:45 +0900

    リングバッファのより詳しい理解と実装の為にCでのQueueの回答をコメントアウトで記述。

--- algorithm_and_data_structure/Aizu_Online_Judge/データ構造()/Queue.cpp
+++ algorithm_and_data_structure/Aizu_Online_Judge/データ構造()/Queue.cpp
@@ -73,3 +73,66 @@ void Queue() {
     }
 }
 
+/**
+ 本当はc++で解いた方が良いが解説がcで書かれていてまたリングバッファの理解の為には
+ cで書いた方が良さそうなので念の為、cでの回答を記載する。後で見直す。
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #define LEN 100005
+
+ typedef struct pp{
+     char name[100];
+     int t;
+ }P;
+
+ P Q[LEN];
+ int head,tail,n;
+
+ void enqueue(P x)
+ {
+     Q[tail]=x;
+     tail=(tail+1)%LEN;
+ }
+
+ P dequeue(){
+     P x=Q[head];
+     head=(head+1)%LEN;
+     return x;
+ }
+
+ int min(int a,int b)
+ {
+     return a<b?a:b;
+ }
+
+ int main(int argc, const char * argv[]) {
+     // insert code here...
+     int elaps=0,c;
+     int i,q;
+     P u;
+     scanf("%d %d",&n,&q);
+     for(i=1;i<=n;i++)
+     {
+         scanf("%s",Q[i].name);
+         scanf("%d",&Q[i].t);
+     }
+     head=1;
+     tail=n+1;
+     while(head!=tail)
+     {
+         u=dequeue();
+         c=min(q,u.t);
+         u.t-=c;
+         elaps+=c;
+         if(u.t>0)
+             enqueue(u);
+         else
+         {
+             printf("%s %d\n",u.name,elaps);
+         }
+     }
+     return 0;
+ }
+
+ */
