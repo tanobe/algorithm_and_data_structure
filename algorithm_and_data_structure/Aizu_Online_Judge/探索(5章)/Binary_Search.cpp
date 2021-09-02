462dbe63ac76565b91f81c76209ae85b81c60523
kaiTanobe <stomp0630@icloud.com>
2021-09-02 16:48:49 +0900

    Binary_Searchファイル作成

---
+++ algorithm_and_data_structure/Aizu_Online_Judge/探索(5章)/Binary_Search.cpp
@@ -0,0 +1,13 @@
+//
+//  Binary_Search.cpp
+//  algorithm_and_data_structure
+//
+//  Created by 田野辺開 on 2021/09/02.
+//
+
+#include <iostream>
+using namespace std;
+
+void Binary_Search() {
+    
+}
